2ad9a9747c6741c9ea36789627725df429d8e32c
lukas.licek <lukas.licek@artin.cz>
2021-04-24 12:35:43 +0200

    Camera follow.

---
+++ LD48/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public KeyCode zoomOutKey = KeyCode.F;
+
+    public float smoothSpeed = 0.125f;
+    private Vector3 offset;
+
+    public float zoomedOutFov = 120f;
+    public float zoomOutSpeed = 60f;
+    private float startingFOV;
+
+    private void Awake()
+    {
+        startingFOV = Camera.main.fieldOfView;
+        offset = transform.position - target.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        var targetPosition = target.position + offset;
+        var smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
+        transform.position = smoothedPosition;        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(zoomOutKey))
+        {
+            Camera.main.fieldOfView = Mathf.Min(zoomedOutFov, Camera.main.fieldOfView + zoomOutSpeed * Time.deltaTime);
+        }
+        else if (Camera.main.fieldOfView > startingFOV)
+        {
+            Camera.main.fieldOfView = Mathf.Max(startingFOV, Camera.main.fieldOfView - zoomOutSpeed * Time.deltaTime);
+        }
+    }
+}
